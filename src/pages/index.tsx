3a45acee7cfc2fffc50101f0d1bde2eacfac90c4
Andrêves Dickow <andrevesdickow@MacBook-Air-de-Andreves.local>
2022-02-08 14:52:30 -0300

    - desenvolvimento do desafio: Interface com Chakra UI

---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import { GetStaticProps } from "next"
+import Head from "next/head"
+import { Flex, Heading } from "@chakra-ui/react"
+import Prismic from '@prismicio/client'
+
+import { Banner } from "../components/Banner"
+import { Characteristics } from "../components/Characteristics"
+import { Header } from "../components/Header"
+import { Separador } from "../components/Separador"
+import { Slider } from "../components/Slider"
+import { getPrismicClient } from "../services/prismic"
+
+interface HomeProps {
+  continents: {
+    slug: string;
+    title: string;
+    summary: string;
+    image: string;
+  }[]
+}
+
+export default function Home({ continents }: HomeProps) {
+  return (
+    <Flex direction="column">
+      <Head>
+        <title>WorldTrip - Home</title>
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta property="og:title" content="WorldTrip" />
+        <meta name="twitter:title" content="WorldTrip" />
+      </Head>
+
+      <Header />
+      <Banner />
+      <Characteristics />
+      <Separador />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg",
+          "3xl",
+          "4xl"]}
+      >
+        Vamos nessa?<br />Então escolha seu continente
+      </Heading>
+
+      <Slider continents={continents} />
+    </Flex>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'continent')]
+  )
+
+  const continents = response.results.map(continent => {
+    return {
+      slug: continent.uid,
+      title: continent.data.title,
+      summary: continent.data.summary,
+      image: continent.data.slider_image.url
+    }
+  })
+
+  return {
+    props: {
+      continents
+    }
+  }
+}
